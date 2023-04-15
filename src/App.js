4e0fc187d05daa908093c45b895db24bbf0f606a
yedukondalu <yedukondalunakkala7@gmail.com>
2023-04-15 14:15:59 +0000

    Nxt Slides

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css'
 
 import NxtSlides from './components/NxtSlides'
 
+// ccbp start RJSCPFQLEO
+
+// ccbp submit RJSCPFQLEO
+
+// ccbp publish RJSCPFQLEO domain_name.ccbp.tech
+
 const App = () => <NxtSlides />
 
 export default App
